27aa2cc5784c2240f5eb0c3a390031600e697641
arotmistrenko <arotmistrenko@gmail.com>
2018-07-30 12:03:31 +0300

    Added: Admin page with Convers modules listing

---
+++ convers/common/src/Controller/Admin/ModulesPageController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\common\Controller\Admin\ModulesPageController.
+ */
+
+namespace Drupal\common\Controller\Admin;
+
+use Drupal\system\Controller\SystemController;
+
+class ModulesPageController extends SystemController
+{
+    public function content()
+    {
+        return $this->systemAdminMenuBlockPage();
+    }
+}
